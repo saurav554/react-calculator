a4f128c0ad5d21b79661e6d0233be459b0881bfd
saurav <avisaurav485@gmail.com>
2021-03-25 01:56:50 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,84 +1,119 @@
 import React, { Component } from 'react';
-import './App.css';
-import ResultComponent from './components/ResultComponent';
-import KeyComponent from "./components/KeyComponent";
+import InvoiceDetail from './components/InvoiceDetail';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
 
 class App extends Component {
-    constructor(){
-        super();
-
-        this.state = {
-            result: ""
-        }
-    }
-
-    onClick = button => {
-
-        if(button === "="){
-            this.calculate()
-        }
-
-        else if(button === "C"){
-            this.reset()
-        }
-        else if(button === "CE"){
-            this.backspace()
-        }
-
-        else {
-            this.setState({
-                result: this.state.result + button
-            })
-        }
+  constructor(){
+    super();
+    this.state={
+      invoiceNumber:'',
+      date:'23-3-2021',
+      billInfo:'',
+       invoiceItems:[{
+         itemName:'',
+         itemsQuantity:'',
+         itemRate:''
+        //  edit:'false'
+       }]
     };
+    this.handleOnChange=this.handleOnChange.bind(this);
+    
+  }
+   addNewItem=()=>{
+    this.setState({
+      invoiceItems:[...this.state.invoiceItems,{itemName:'', itemsQuantity:0, itemRate:0 }]
+    });
+  }
 
 
-    calculate = () => {
-        var checkResult = ''
-        if(this.state.result.includes('--')){
-            checkResult = this.state.result.replace('--','+')
-        }
+  
+  deleteItem=(ind)=>{
+    let item=[...this.state.invoiceItems];
+    this.state.invoiceItems.map((invoice, index)=>{
+      if(index===ind){
+        item.splice(ind, 1); 
+      }
+    })
+    this.setState({
+      invoiceItems:[...item ]
+     }) 
+  }
 
-        else {
-            checkResult = this.state.result
-        }
 
-        try {
-            this.setState({
-                // eslint-disable-next-line
-                result: (eval(checkResult) || "" ) + ""
-            })
-        } catch (e) {
-            this.setState({
-                result: "error"
-            })
+  // editItem=(ind)=>{
+  //   let item=[...this.state.invoiceItems];
+  //   this.state.invoiceItems.map((invoice, index)=>{
+  //     if(index===ind){
+  //       item.splice(ind, 1); 
+  //     }
+  //   })
+  //   this.setState({
+  //     invoiceItems:[...item ]
+  //    }) 
+  // }
 
-        }
-    };
+  
+  convertPdf=()=>{
+    var pdfConverter = require('jspdf');
+    let totalAmount=0;
+    var doc = new pdfConverter.jsPDF();
+    
+    doc.setLineWidth(12);
+    doc.setDrawColor(44, 68, 71);
+    doc.line(210, 70, 0, 70);
+    doc.setTextColor(256)  
+    doc.text(10,72, "Item");
+    doc.text(95,72, "Quantity");
+    doc.text(135,72, "Rate");
+    doc.text(175,72, "Amount");
+    doc.setTextColor(0) 
+    this.state.invoiceItems.map((item,index)=>{
+    totalAmount+=item.itemRate*item.itemsQuantity;
+    doc.text(10,82 +index *8, item.itemName);
+    doc.text(95,82 +index *8, ""+item.itemsQuantity);
+    doc.text(135,82 +index *8, "$."+item.itemRate);
+    doc.text(175,82 +index *8, "$."+item.itemRate*item.itemsQuantity);
+    })
+    doc.text(100,150, "Subtotal:  "+' $.'+totalAmount);
+    doc.text(100,160, "TAX (10%):  "+' $.'+totalAmount*10/100);
+    doc.text(100,170, "Total:  "+' $.' +((totalAmount*10/100)+ totalAmount) );
+    doc.save("test.pdf");
 
-    reset = () => {
-        this.setState({
-            result: ""
-        })
-    };
+            
+  }
+  handleOnChange=(event , ind, Name)=>{    
+    let item=[...this.state.invoiceItems];       
+    this.state.invoiceItems.map((invoice , index)=>{
+            if(index==ind ){   
+              if(Name=="name")           
+               item[index].itemName=event.target.value;
+               if(Name=="Quan")           
+               item[index].itemsQuantity=event.target.value;
+               if(Name=="Rate")           
+               item[index].itemRate=event.target.value;              
+            }
+          });     
+         this.setState({
+          invoiceItems:[...item ]
+         })         
+  }
 
-    backspace = () => {
-        this.setState({
-            result: this.state.result.slice(0, -1)
-        })
-    };
+  render() {
+    return (
+      <div className="container">
+        <InvoiceDetail pdf={this.convertPdf} 
+          items={this.state.invoiceItems} 
+           event={this.addNewItem} 
+          ItemName={this.handleItemName}
+           del={this.deleteItem} 
+          //  edit={this.editItem}
+           onChangeHandler={this.handleOnChange} 
 
-    render() {
-        return (
-            <div>
-                <div className="calculator-body">
-                    <h1>Calculator</h1>
-                    <ResultComponent result={this.state.result}/>
-                    <KeyComponent onClick={this.onClick}/>
-                </div>
-            </div>
-        );
-    }
+           />
+       
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/invoiceDetail.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import AddMoreInvoices from'./AddInvoices';
+function InvoiceDetails(props){
+
+    return(        
+        <div className="card">
+            <h5 className="card-header text-center">BILL DETAILS</h5>
+          <div className="card-body">
+            <form >
+                {
+                     props.items.map(
+                        (key ,index)=>{
+                            return <AddMoreInvoices  item={key} ind={index}  del={props.del} 
+                            
+                            onChangeHandler={props.onChangeHandler}/>
+                        }
+                    )
+                }                
+            </form>
+            <div className="btn btn-primary" 
+            style={{float:'right', marginRight:'100px'}} 
+            onClick={props.event}>Add new</div>
+           </div>
+
+          <div className="row">
+          <div className="col-sm-12 mt-5">
+          <div className="btn btn-primary col-sm-6 sol-xs-12 mt-5" 
+          onClick={props.pdf} 
+          style={{ margin:'20px auto', display:'block'}}> Generate PDF</div>
+      </div>
+          </div>
+    </div>
+    );
+}
+export default InvoiceDetails;
